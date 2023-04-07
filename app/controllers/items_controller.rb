b435fef9f7e4c1f0e47dd0750245126a58028973
kotalo122 <kotalo122@gmail.com>
2023-04-07 21:12:45 +0900

    itemsマイグレーション、active strage　導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,21 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy,]
 
   def index
+
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
     
   end
+
+  # private
+  # def item_params
+  #   params.require(:item).permit(:image, :text).merge(user_id: current_user.id)
+  # end
+
 end
