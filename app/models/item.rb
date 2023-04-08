b1eaa7a4b17ad6a230f5871f1634431555b2719b
kotalo122 <kotalo122@gmail.com>
2023-04-09 02:42:02 +0900

    Active_hash導入、価格設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,14 +3,25 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :discription
-    validates :category_id
-    validates :condition_id
-    validates :price
-    validates :shipping_fee_id
-    validates :prefecture_id
-    validates :ship_day_id
+    validates :category_id,     numericality: { other_than: 1 , message: "can't be blank"}
+    validates :condition_id,    numericality: { other_than: 1 , message: "can't be blank"}
+    validates :price, numericality: {
+      greater_than_or_equal_to: 300,
+      less_than_or_equal_to: 9_999_999,
+      message: 'must be between ¥300 and ¥9,999,999'
+    }, format: { with: /\A[0-9]+\z/, message: 'must be a half-width number' }
+    validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :prefecture_id,   numericality: { other_than: 1 , message: "can't be blank"}
+    validates :ship_day_id,     numericality: { other_than: 1 , message: "can't be blank"}
   end
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :ship_day
+  
   belongs_to :user
   has_one_attached :image
 
