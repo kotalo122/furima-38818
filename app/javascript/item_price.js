936d02412114093f40691cb67f4fa2a59f877ece
kotalo122 <kotalo122@gmail.com>
2023-04-10 00:53:05 +0900

    javasprict　実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,15 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("keyup", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    const TAX_RATE = 0.1;
+    const taxPrice = Math.round(inputValue * TAX_RATE); 
+    addTaxDom.innerHTML = ` ${taxPrice} `;
+    
+    const addProfitDom = document.getElementById("profit");
+    const salesProfit = Math.round(inputValue - taxPrice); 
+    addProfitDom.innerHTML = ` ${salesProfit} `;
+  });
 });
