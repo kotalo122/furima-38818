e8279063a788fb66784fef6f01de768cd9e82416
kotalo122 <kotalo122@gmail.com>
2023-04-22 00:32:26 +0900

    ordersコントローラー実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,3 +1,32 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+  before_action :move_to_index, only: [:index, :create]
+
+  def index
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    binding.pry
+    @order = Order.create(order_params)
+    Shipment.create(shipment_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:user_id, :item_id)
+  end
   
+  def shipment_params
+    params.require(:shipment).permit(:order_id, :address, :post_code, :prefecture_id, :city, :block, :building, :phone).merge(order_id: @order.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    if current_user == @item.user
+      redirect_to item_path(@item.id)
+    end
+  end
 end
--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,7 +1,7 @@
 class Shipment < ApplicationRecord
   
   with_options presence: true do
-    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Postal code is invalid. Enter it as follows (e.g. 123-4567)' }
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id,   numericality: { other_than: 1 , message: "Prefecture can't be blank"}
     validates :city
     validates :block
