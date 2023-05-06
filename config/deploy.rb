d9edbecf8853ac3544b2dc88b142dc71d5bdfac3
kotalo122 <kotalo122@gmail.com>
2023-05-07 01:53:17 +0900

    自動デプロイのためCapistranoの導入

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+lock '3.17.2'
+
+set :application, 'furima-38818'
+
+set :repo_url,  'git@github.com:kotalo122/furima-38818.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/furima38818kotalo122.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
